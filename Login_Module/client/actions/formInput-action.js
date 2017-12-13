6c532342acff3035434ac954eefd5598308bfb7b
Dwivedi <577730@cognizant.com>
2017-12-13 17:06:45 +0530

    login_module

---
+++ Login_Module/client/actions/formInput-action.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import {browserHistory} from 'react-router';
+
+export const formInputData = (input) => {
+    return dispatch => {
+
+        axios.post("http://localhost:3000/userdetail", {
+            type: input.productType,
+            description: input.productDescription
+        })
+            .then((response) => {
+                console.log("post ************", response.data);
+                alert("Error Sent successfully")
+                browserHistory.push('/timeline');
+            })
+            .catch((error) => {
+                //    console.log(error);
+            })
+
+    }
+
+}
