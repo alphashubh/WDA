6126ebfce847dbde32fcb2aa06b52136bf0cba3c
Dwivedi <577730@cognizant.com>
2018-03-21 11:40:03 +0530

    basic structure

---
+++ BS/client/component/footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>xyz.com</p>
+            <ul >
+                <li><Link to="/"><span class="glyphicon glyphicon-home" aria-hidden="true"></span> Home</Link></li>
+                <li><Link to="/about"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> About Us</Link></li>
+                <li><Link to="/contact"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span> Contact Us</Link></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ BS/client/component/jsonList.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class JsonList extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      data: []
+    }
+
+
+  }
+  componentDidMount() {
+
+    axios.get('http://www.json-generator.com/api/json/get/ceqpXZevEy?indent=2')
+      .then(list => {
+        console.log(list);
+
+
+        this.setState({ data: list.data })
+        console.log("333333333333333333333333", list.data);
+
+      })
+      .catch(error => {
+        console.log("Error in axios", error);
+      });
+
+  }
+
+  craeteList() {
+    return this.state.data.map((item) => {
+      return (
+        <tr>
+          <td>{item.age}</td>
+          <td>{item.eyeColor}</td>
+          <td>{item.name}</td>
+          <td>{item.longitude}</td>
+        </tr>
+      )
+
+    })
+  }
+
+
+  render() {
+
+    return (
+      <div>
+        <table className="table">
+          <th>AGE</th>
+          <th>EYE COLOR</th>
+          <th>NAME</th>
+          <th>LONGITUDE</th>
+          {this.craeteList()}
+        </table>
+      </div>
+
+    )
+  }
+}
